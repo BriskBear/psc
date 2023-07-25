4e2b7111491518d92990bbc56ee9be1ff12c4722
deftclaw <deftclaw@briskbear.rocks>
2023-07-24 18:12:10 -0600

    Ruby prompt

--- ps1.sh
+++ ps1.sh
@@ -1,5 +1,5 @@
-# Set the shell prompt : 1688248605
+# Set the shell prompt : 1690243848
 PS1='\u'                      #  Display Username
 PS1+='\[$( errname $? )\]'    #  Display ]|[ or error code, random-color hostname
 PS1+=':\w'                    #  Display current directory
-PS1+='\n\[$( glyph ">>" )\] ' #  Display uniform-length prompt-icon
+PS1+='\n\[$( glyph " " )\] ' #  Display uniform-length prompt-icon

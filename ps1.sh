137c1a55d3c08ba229e6be5e97342a74765ce7bf
deftclaw <eeccher@cppwind.com>
2023-10-30 13:08:46 -0600

    Bracket container user in green

--- ps1.sh
+++ ps1.sh
@@ -1,5 +1,5 @@
 # Set the shell prompt : 1690243848
-PS1='\u'                      #  Display Username
-PS1+='\[$( errname $? )\]'    #  Display ]|[ or error code, random-color hostname
-PS1+=':\w'                    #  Display current directory
-PS1+='\n\[$( glyph " " )\] ' #  Display uniform-length prompt-icon
+PS1='[\033[38;5;47m\u\033[0m]' #  Display Username
+PS1+='\[$( errname $? )\]'     #  Display ]|[ or error code, random-color hostname
+PS1+=':\w'                     #  Display current directory
+PS1+='\n\[$( glyph " " )\] '  #  Display uniform-length prompt-icon

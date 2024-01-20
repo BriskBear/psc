22342abe259930a34dbc2a2f8f3af38fdc016438
deftclaw <deftclaw@briskbear.rocks>
2024-01-19 22:03:03 -0700

    Update variables to more clearly indicate purpose

--- glyph.c
+++ glyph.c
@@ -5,10 +5,10 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  char * err = argv[1];   // get the error code from the command line
+  char * sym = argv[1];   // get the glyph [sym]bol from the command line
   srand(time(NULL));      // seed pseudo-random with time
   int hue = rand() % 255; // random color
 
-  printf("\033[38;5;%dm%s\033[0m", hue, err);
+  printf("\033[38;5;%dm%s\033[0m", hue, sym);
   return 0;
 }

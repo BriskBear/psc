e7db1aff8006943fa3677ee5094329a7ad7f5aa4
deftclaw <deftclaw@briskbear.rocks>
2024-01-19 22:09:37 -0700

    Rubys are red, this makes the glyph always red

--- glyph.c
+++ glyph.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-#include <unistd.h>
 
 int main(int argc, char *argv[]) {
   char * sym = argv[1];   // get the glyph [sym]bol from the command line
-  srand(time(NULL));      // seed pseudo-random with time
-  int hue = rand() % 255; // random color
 
-  printf("\033[38;5;%dm%s\033[0m", hue, sym);
+  printf("\033[38;5;160m%s\033[0m", sym);
   return 0;
 }
